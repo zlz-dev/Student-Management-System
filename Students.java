f408130844d500a858afd8e2fbf9fb5dd8f066f3
zhou lanzhao <768790466@qq.com>
2021-01-02 15:39:55 +0900

    first commit

---
+++ Students.java
@@ -0,0 +1,18 @@
+public class Students {
+
+    
+    //Constructor:prompt user to enter student's name and year
+
+    //Generate an ID
+    
+    //Enroll in courses
+
+    //View balance
+
+    //Pay tuition
+
+    //Show status
+
+
+
+}
